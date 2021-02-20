6b6c0038ca75f6aa68b54bd9c19ce38c13b61cb7
ayx666 <1613779069@qq.com>
2021-02-20 10:40:40 +0800

    第一次提交交

---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //导入qss文件
+    QFile file(":/url/3.qss");
+    if(file.open(QFile::ReadOnly)){
+        qDebug()<<"qss文件打开成功";
+        QString sty=QString::fromLatin1(file.readAll());
+        this->setStyleSheet(sty);
+    }
+    file.close();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
